03dabea12226f688c1e9ea100d1b6651905e8ccf
cul314 <cul314@cul314-pc.wireless-1x.unc.edu>
2019-08-28 13:10:57 -0400

    A1 Novice complete: version 1

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,44 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Number of customers
+		int count = scan.nextInt();
+		
+		char[] firstInitial = new char[count];
+		String[] lastName = new String[count];
+		double[] total = new double[count];
+		
+		// Retrieve input and store in arrays
+		
+		for(int i = 0; i < count; i++) {
+			firstInitial[i] = scan.next().charAt(0);
+			lastName[i] = scan.next();
+			
+			// The number of different items
+			int itemCount = scan.nextInt();
+			
+			for(int j = 0; j < itemCount; j++) {
+				// The amount of a particular item being purchased
+				int quantity = scan.nextInt();
+				
+				// Skip the item name
+				scan.next();
+				
+				// The cost of the individual item
+				double price = scan.nextDouble();
+				
+				total[i] += quantity * price;
+			}
+		}
+		
+		// Display name of each customer and the amount spent to the console
+		
+		for(int i = 0; i < count; i++) {
+			System.out.printf("%c. %s: %.2f%n", firstInitial[i], lastName[i], total[i]);
+		}
+		
+		// Close Scanner object
+		scan.close();
+		
 	}
 }
