012899a0b9df67dd3abf00adc263f0a42103aa60
cul314 <cul314@cul314-pc.wireless-1x.unc.edu>
2019-09-01 13:01:37 -0400

    A1 Jedi version 1

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,57 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		int itemCount = scan.nextInt();
+		String[] itemNames = new String[itemCount];
+		
+		// Store item names and prices in arrays
+		for (int i = 0; i < itemCount; i++) {
+			itemNames[i] = scan.next();
+			scan.nextDouble();
+		}
+		
+		int customerCount = scan.nextInt();
+		int[] customersPurchased = new int[itemCount]; // Number of different customers who bought a certain item
+		int[] quantityPurchased = new int[itemCount];  // Total number of a certain type of item that was purchased
+		
+		// Calculate number of customers who bought each item and the total quantity bought of each item.
+		for (int i = 0; i < customerCount; i++) {
+			// Skip the customers names
+			scan.next();
+			scan.next();
+			
+			int numberOfItemsBought = scan.nextInt();
+			
+			for (int j = 0; j < numberOfItemsBought; j++) {
+				int quantity = scan.nextInt();
+				int itemIndex = getItemIndex(itemNames, scan.next());
+				
+				customersPurchased[itemIndex] += 1;
+				quantityPurchased[itemIndex] += quantity;
+			}
+		}
+		
+		// Display number of customers who bought each item and the total quantity bought of each item. 
+		for (int i = 0; i < itemCount; i++) {
+			if(customersPurchased[i] > 0) {
+				System.out.println(customersPurchased[i] + " customers bought " + quantityPurchased[i]
+						+ " " + itemNames[i]);
+			} else {
+				System.out.println("No customers bought " + itemNames[i]);
+			}
+		}
+		
+		// Close the Scanner Object
+		scan.close();
+	}
+	
+	// Returns the index of the String value in array names corresponding to the parameter String item
+	static int getItemIndex(String[] names, String item) {
+		for (int i = 0; i < names.length; i++) {
+			if (item.equals(names[i])) {
+				return i;
+			}
+		}
+		return 0;
 	}
 }
